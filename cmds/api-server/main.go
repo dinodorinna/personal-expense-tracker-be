2564af53800ede6fcd056c36e09a852af8180891
dinodorinna <somfanc@gmail.com>
2025-07-06 19:09:19 +0700

    update : allow cors

--- cmds/api-server/main.go
+++ cmds/api-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/dinodorinna/personal-expense-tracker-be/internal/handler"
 	"github.com/dinodorinna/personal-expense-tracker-be/internal/repo"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,10 +12,10 @@ func main() {
 	repo.SetupDatabase()
 
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	r.GET("/transactions", handler.GetTransactionsHandler)
 	r.POST("/transaction", handler.CreateTransactionHandler)
 	r.DELETE("/delete/:id", handler.DeleteTransactionHanler)
-
 	r.Run()
 }
